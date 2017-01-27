318a06071b31bd094f67d72f355837c51bb01a44
Wallace Wylie <wallace.wylie@gmail.com>
2017-01-27 11:45:25 -0600

    New update to statement

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -17,15 +17,15 @@ public class Main {
 		for(MathEquation equation : equations) {
 			equation.execute();
 			System.out.print("Result = ");
-			System.out.println(equation.result);
+			System.out.println(equation.getResult());
 		}
     }
 
     public static MathEquation create(double leftVal, double rightVal, char opCode) {
     	MathEquation equation = new MathEquation();
-    	equation.leftVal = leftVal;
-    	equation.rightVal = rightVal;
-    	equation.opCode = opCode;
+    	equation.setLeftVal(leftVal);
+    	equation.setRightVal(rightVal);
+    	equation.setOpCode(opCode);
 
     	return equation;
 	}
